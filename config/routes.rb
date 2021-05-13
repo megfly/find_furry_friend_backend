d0aa9ebba818a765023321e56dca012dcb0b9875
megfly <mflynn333@gmail.com>
2021-05-12 21:53:52 -0400

    setup initial routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do 
+      
+      # get '/shelters' => 'shelters#index' 
+      # get, post, patch delete
+
+      resources :shelters do 
+        resources :pets 
+        
+      end 
+    end 
+  end 
 end
