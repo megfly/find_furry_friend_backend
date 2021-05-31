8b2fbdc0f43ac68f3662e17937a1f518541fe241
megfly <mflynn333@gmail.com>
2021-05-31 17:50:08 -0400

    removed seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,17 +7,17 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
 
-3.times do
-    Shelter.create(
-      name: Faker::Company.name,
-      street_address: Faker::Address.street_address,
-      city: Faker::Address.city,
-      state: Faker::Address.state,
-      zipcode: Faker::Address.zip_code,
-      email: Faker::Internet.email,
-      phone: Faker::PhoneNumber.phone_number
-    )
-  end
+# 3.times do
+#     Shelter.create(
+#       name: Faker::Company.name,
+#       street_address: Faker::Address.street_address,
+#       city: Faker::Address.city,
+#       state: Faker::Address.state,
+#       zipcode: Faker::Address.zip_code,
+#       email: Faker::Internet.email,
+#       phone: Faker::PhoneNumber.phone_number
+#     )
+#   end
 
 #   3.times do 
 #     Pet.create(
