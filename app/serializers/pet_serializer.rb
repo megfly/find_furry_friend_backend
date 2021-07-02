b4b09499fddb51550f0d9fc4a9fc5dc527725e3e
megfly <mflynn333@gmail.com>
2021-07-02 14:05:58 -0400

    added spacing

--- app/serializers/pet_serializer.rb
+++ app/serializers/pet_serializer.rb
@@ -2,4 +2,5 @@ class PetSerializer < ActiveModel::Serializer
   belongs_to :shelter
   attributes :id, :shelter_id, :name, :animal_type, :breed, :size, :gender, :age, :color,
   :image, :good_with_children, :good_with_dogs, :good_with_cats, :house_trained, :vaccinated
+  
 end
