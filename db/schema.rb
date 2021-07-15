bb4eb1d109df98404087a645f4a9ee7830e4923f
megfly <mflynn333@gmail.com>
2021-07-15 11:34:32 -0400

    added shelter_id to shelters table and migrated db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_03_204521) do
+ActiveRecord::Schema.define(version: 2021_07_15_153014) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 2021_05_03_204521) do
     t.string "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "shelter_id"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
