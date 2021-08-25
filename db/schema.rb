4b6b197fadf0625c4585692f25614685bae2b581
megfly <mflynn333@gmail.com>
2021-08-24 22:13:58 -0400

    attempted to remove shelters foreign key form pets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_15_153014) do
+ActiveRecord::Schema.define(version: 2021_08_25_020523) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,5 +70,4 @@ ActiveRecord::Schema.define(version: 2021_07_15_153014) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "pets", "shelters"
 end
